f919d23f5ed2e2951f6386a1620815a268fa7ee6
shakilahmmeed <shakilahmmeed@gmail.com>
2021-01-18 10:02:37 +0600

    feat: delete task from history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -2,64 +2,72 @@ import React, { useState, useEffect } from "react";
 import { Table, Button } from "antd";
 import Page from "../components/Page";
 
-const columns = [
-	{
-		title: "Title",
-		dataIndex: "title",
-		key: "title",
-		render: (title) => <p>{title}</p>,
-	},
-	{
-		title: "Deadline",
-		dataIndex: "date",
-		key: "date",
-		render: (date) => (
-			<p>
-				{new Date(date).toLocaleString("default", {
-					day: "numeric",
-					month: "long",
-				})}
-			</p>
-		),
-	},
-	{
-		title: "Started At",
-		dataIndex: "range",
-		key: "range",
-		render: (date) => {
-			let msg = date[0] ?? "No Date";
-			return <p>{msg}</p>;
-		},
-	},
-	{
-		title: "End At",
-		dataIndex: "range",
-		key: "range",
-		render: (date) => {
-			let msg = date[1] ?? "No Date";
-			return <p>{msg}</p>;
-		},
-	},
-	{
-		title: "Action",
-		key: "action",
-		render: (title) => (
-			<Button type="primary" danger>
-				Delete
-			</Button>
-		),
-	},
-];
-
 export default function History() {
-	const [completedTodos, setCompletedTodos] = useState([]);
+	const [todos, setTodos] = useState([]);
+
+	const columns = [
+		{
+			title: "Title",
+			dataIndex: "title",
+			key: "title",
+			render: (title) => <p>{title}</p>,
+		},
+		{
+			title: "Deadline",
+			dataIndex: "date",
+			key: "date",
+			render: (date) => (
+				<p>
+					{new Date(date).toLocaleString("default", {
+						day: "numeric",
+						month: "long",
+					})}
+				</p>
+			),
+		},
+		{
+			title: "Started At",
+			dataIndex: "range",
+			key: "range",
+			render: (date) => {
+				let msg = date[0] ?? "No Date";
+				return <p>{msg}</p>;
+			},
+		},
+		{
+			title: "End At",
+			dataIndex: "range",
+			key: "range",
+			render: (date) => {
+				let msg = date[1] ?? "No Date";
+				return <p>{msg}</p>;
+			},
+		},
+		{
+			title: "Action",
+			key: "action",
+			dataIndex: "id",
+			render: (id) => (
+				<Button
+					type="primary"
+					danger
+					onClick={() => {
+						let filteredTodoList = todos.filter((todo) => todo.id !== id);
+						localStorage.setItem("todos", JSON.stringify(filteredTodoList));
+						setTodos(filteredTodoList);
+					}}
+				>
+					Delete
+				</Button>
+			),
+		},
+	];
 
 	useEffect(() => {
 		let todos = JSON.parse(localStorage.getItem("todos"));
 
 		if (todos !== null) {
-			let filteredTodos = todos.filter((todo) => todo.status === "completed");
-			setCompletedTodos(filteredTodos);
+			setTodos(todos);
 		}
 	}, []);
 
@@ -67,7 +75,10 @@ export default function History() {
 		<Page>
 			<div className="mt-28 flex justify-center">
 				<div>
-					<Table columns={columns} dataSource={completedTodos} />
+					<Table
+						columns={columns}
+						dataSource={todos.filter((todo) => todo.status === "completed")}
+					/>
 				</div>
 			</div>
 		</Page>
