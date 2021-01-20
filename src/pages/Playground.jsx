1c7587401b7fb2126e9a60bb117df8019fdd61d7
shakilahmmeed <shakilahmmeed@gmail.com>
2021-01-20 16:12:19 +0600

    fix: netlify deploy issue is fixed

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,26 +1,36 @@
+// import React from "react";
+// import {
+// 	Switch,
+// 	Route,
+// 	Link,
+// 	useHistory,
+// 	useRouteMatch,
+// } from "react-router-dom";
+// import { Button } from "antd";
+
+// export default function Playground() {
+// 	let history = useHistory();
+// 	let match = useRouteMatch();
+
+// 	const handleClick = () => {
+// 		console.log(match);
+// 	};
+
+// 	return (
+// 		<div>
+// 			<Button type="primary" onClick={handleClick}>
+// 				Click Me
+// 			</Button>
+// 		</div>
+// 	);
+// }
+
 import React from "react";
-import {
-	Switch,
-	Route,
-	Link,
-	useHistory,
-	useRouteMatch,
-} from "react-router-dom";
-import { Button } from "antd";
 
 export default function Playground() {
-	let history = useHistory();
-	let match = useRouteMatch();
-
-	const handleClick = () => {
-		console.log(match);
-	};
-
 	return (
 		<div>
-			<Button type="primary" onClick={handleClick}>
-				Click Me
-			</Button>
+			<h1>this is playground</h1>
 		</div>
 	);
 }
